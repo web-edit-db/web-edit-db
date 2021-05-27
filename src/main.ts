f0c07728807e3b8966a0283af6933cde25fe6278
Hanan F <hananfokkens@gmail.com>
2021-05-27 17:40:27 +1200

    setup tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
+import './assets/tailwind.css'
+import '@fontsource/inter/variable.css'
 import router from './router'
 
 createApp(App).use(router).mount('#app')
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const colors = require('tailwindcss/colors')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ['Inter', ...defaultTheme.fontFamily.sans]
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+
+      black: colors.black,
+      white: colors.white,
+      gray: colors.blueGray,
+      red: colors.red,
+      yellow: colors.amber,
+      green: colors.emerald,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      purple: colors.violet,
+      pink: colors.pink
+    },
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
