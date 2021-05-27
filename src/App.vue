42007a5f5f11fcb5465943ffbae5cd8cd5b489ac
Hanan F <hananfokkens@gmail.com>
2021-05-27 17:55:07 +1200

    added basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,50 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <aside>
+    <header>{{ database.name }}</header>
+    <main></main>
+    <footer>
+      <span>Open Database</span>
+    </footer>
+  </aside>
   <router-view/>
 </template>
 
-<style>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup () {
+    return {
+      database: {
+        name: 'Database Name'
+      }
+    }
+  }
+})
+</script>
+
+<style lang="postcss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  @apply h-screen;
+  @apply flex;
+  @apply bg-gray-100;
 }
 
-#nav {
-  padding: 30px;
+#app > aside {
+  @apply w-60;
+  @apply bg-white;
+  @apply rounded-3xl;
+  @apply shadow-md;
+  @apply flex flex-col;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+#app > aside header {
+  @apply p-4;
+  @apply text-xl;
+  @apply font-extrabold;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+#app > aside main {
+  @apply flex-grow;
 }
 </style>
