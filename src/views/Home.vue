998aea00ff730792de9645841c7e9c463adf2d56
Hanan F <hananfokkens@gmail.com>
2021-09-02 20:19:50 +1200

    improved home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,10 @@
       alt="Vue logo"
       src="../assets/logo.png"
     >
+    <h1>Web Edit DB</h1>
+    <p>
+      Web Edit DB is a free browser-based SQLite database editor.
+    </p>
   </div>
 </template>
 
@@ -14,3 +18,19 @@ export default defineComponent({
   name: 'Home'
 })
 </script>
+
+<style lang="postcss" scoped>
+.home {
+  @apply flex flex-col justify-center items-center gap-3;
+  @apply h-full;
+}
+
+img {
+  @apply h-20;
+}
+
+h1 {
+  @apply text-2xl;
+  @apply text-primary;
+}
+</style>
