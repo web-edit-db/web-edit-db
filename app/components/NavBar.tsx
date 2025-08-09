5e5b3deac132c3eeb6c5144b68ac89993d92b642
Hanan Fokkens <hananfokkens@gmail.com>
2025-08-10 01:25:09 +1200

    Add layout and navigation components for improved routing structure
    - Updated app/routes.ts to include a main layout component.
    - Created app/layout/main.tsx to define the main layout with a NavBar.
    - Added app/components/NavBar.tsx for navigation with a logo.
    - Introduced app/assets/logo.png as the logo asset.

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router";
+import logo from "@/assets/logo.png";
+
+export default function NavBar() {
+    return (
+        <nav className="bg-white dark:bg-gray-800 border-b-2 border-primary h-14 px-3 flex justify-between items-center shadow-lg">
+            <Link to="/" className="flex items-center text-2xl text-primary font-light select-none cursor-pointer">
+                <img src={logo} alt="" className="p-1.5 h-14" />
+                <span className="text-primary">Web Edit DB</span>
+            </Link> 
+        </nav>
+    );
+}
--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,8 @@
-import { type RouteConfig, index, route } from '@react-router/dev/routes'
+import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
 
-export default [index('routes/home.tsx'), route('about', 'routes/about.tsx')] satisfies RouteConfig
+export default [
+    layout("layout/main.tsx", [
+        index("routes/home.tsx"),
+        route("about", "routes/about.tsx"),
+    ])
+] satisfies RouteConfig;
