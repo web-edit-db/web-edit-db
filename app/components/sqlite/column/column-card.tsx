df9ef08e886dc2eff5d92130f2a97ea3441fc53e
Hanan Fokkens <hananfokkens@gmail.com>
2025-08-28 16:05:08 +1200

    feat: add min/max input fields with plus/minus functionality to ColumnCard component
    - Introduced NumberInputWithPlusMinus component for managing numeric inputs with increment and decrement buttons.
    - Updated the ColumnCard form validation schema to include optional min and max properties, enhancing data integrity.
    - Adjusted UI layout for better responsiveness and user experience, ensuring proper rendering of new input fields.
    - Enhanced form handling to trigger validation on min/max changes, improving overall form usability.

--- app/components/sqlite/column/column-card.tsx
+++ app/components/sqlite/column/column-card.tsx
@@ -10,8 +10,8 @@ import {
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { cn } from '@/lib/utils'
-import { IconRotate, IconTrash, IconTrashOff } from '@tabler/icons-react'
-import { useCallback, useMemo, useState } from 'react'
+import { IconMinus, IconPlus, IconRotate, IconTrash, IconTrashOff } from '@tabler/icons-react'
+import { useCallback, useEffect, useMemo, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
@@ -33,6 +33,8 @@ type ColumnData = {
   notNull: boolean
   unique: boolean
   primaryKey: boolean
+  min?: number
+  max?: number
 }
 const columnTypes = ['TEXT', 'INTEGER', 'NUMERIC', 'REAL', 'BLOB']
 
@@ -62,6 +64,8 @@ const ToggleButton = ({
           checked={checked}
           onCheckedChange={onChange}
           disabled={disabled}
+          // don't allow tabbing to the checkbox because we have it in the button
+          tabIndex={-1}
           className="cursor-pointer"
         />
         <span>{label}</span>
@@ -70,16 +74,117 @@ const ToggleButton = ({
   )
 }
 
+const NumberInputWithPlusMinus = ({
+  value,
+  onChange,
+  disabled,
+}: {
+  value: number | undefined
+  onChange: (value: number | undefined) => void
+  disabled?: boolean
+}) => {
+  const [inputValue, setInputValue] = useState(value?.toString() ?? '')
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // prevent the user entering anything that is not a number, -, or ., remove any non-numeric characters
+    const newValue = e.target.value.replace(/[^0-9.-]/g, '')
+    if (newValue === '') {
+      setInputValue('')
+    } else {
+      setInputValue(newValue)
+    }
+  }
+  const onMinus = useCallback(() => {
+    // first try and turn this into a number
+    const newValue = inputValue === '' ? Number.NaN : Number(inputValue)
+    if (Number.isNaN(newValue)) {
+      setInputValue('0')
+    } else {
+      setInputValue((newValue - 1).toString())
+    }
+  }, [inputValue])
+  const onPlus = useCallback(() => {
+    const newValue = inputValue === '' ? Number.NaN : Number(inputValue)
+    if (Number.isNaN(newValue)) {
+      setInputValue('0')
+    } else {
+      setInputValue((newValue + 1).toString())
+    }
+  }, [inputValue])
+  useEffect(() => {
+    const inputValueTrimmed = inputValue.trim()
+    // if this is a an empty string, just a minus
+    if (inputValueTrimmed === '' || inputValueTrimmed === '-') {
+      onChange(undefined)
+    } else {
+      const newValue = Number(inputValueTrimmed)
+      if (Number.isNaN(newValue)) {
+        onChange(undefined)
+      } else {
+        onChange(newValue)
+      }
+    }
+  }, [inputValue, onChange])
+  return (
+    <div className="flex">
+      <Input
+        value={inputValue}
+        onChange={handleInputChange}
+        disabled={disabled}
+        className="-mr-[1px] rounded-r-none focus:z-20"
+      />
+      <Button
+        onClick={onMinus}
+        size="icon"
+        variant="outline"
+        disabled={disabled}
+        className="-ml-[1px] rounded-l-none rounded-r-none focus:z-20"
+      >
+        <IconMinus></IconMinus>
+      </Button>
+      <Button
+        onClick={onPlus}
+        size="icon"
+        variant="outline"
+        disabled={disabled}
+        className="-ml-[1px] rounded-l-none focus:z-20"
+      >
+        <IconPlus></IconPlus>
+      </Button>
+    </div>
+  )
+}
+
 export default function ColumnCard({ columnData }: { columnData: ColumnData }) {
-  const zodSchema = z.object({
-    name: z.string().min(1, { message: 'Name is required' }),
-    type: z.string().refine((value) => columnTypes.includes(value), {
-      message: 'Type is required and must be one of INTEGER, TEXT, or REAL',
-    }),
-    notNull: z.boolean(),
-    unique: z.boolean(),
-    primaryKey: z.boolean(),
-  })
+  const zodSchema = z
+    .object({
+      name: z.string().min(1, { message: 'Name is required' }),
+      type: z.string().refine((value) => columnTypes.includes(value), {
+        message: 'Type is required and must be one of INTEGER, TEXT, or REAL',
+      }),
+      notNull: z.boolean(),
+      unique: z.boolean(),
+      primaryKey: z.boolean(),
+      min: z.number({ message: 'Must be a number' }).optional(),
+      max: z.number({ message: 'Must be a number' }).optional(),
+    })
+    .superRefine((data, ctx) => {
+      if (data.min !== undefined && data.max !== undefined) {
+        const minNum = Number(data.min)
+        const maxNum = Number(data.max)
+        if (!Number.isNaN(minNum) && !Number.isNaN(maxNum) && minNum > maxNum) {
+          ctx.addIssue({
+            code: 'custom',
+            message: 'Min must be less or equal to max',
+            path: ['min'],
+          })
+          ctx.addIssue({
+            code: 'custom',
+            message: 'Max must be greater or equal to min',
+            path: ['max'],
+          })
+        }
+      }
+    })
   const [isDeleted, setIsDeleted] = useState(false)
   const form = useForm<Omit<ColumnData, 'new'>>({
     defaultValues: columnData,
@@ -150,12 +255,13 @@ export default function ColumnCard({ columnData }: { columnData: ColumnData }) {
             </div>
           </div>
         </CardHeader>
-        <CardContent className="grid grid-cols-12 gap-4 px-4">
+        <CardContent className="grid grid-cols-12 items-start gap-4 px-4">
+          {/* Name */}
           <FormField
             control={form.control}
             name="name"
             render={({ field }) => (
-              <FormItem className="col-span-6">
+              <FormItem className="col-span-12 md:col-span-6">
                 <FormLabel>Name</FormLabel>
                 <FormControl>
                   <Input {...field} />
@@ -164,11 +270,12 @@ export default function ColumnCard({ columnData }: { columnData: ColumnData }) {
               </FormItem>
             )}
           />
+          {/* Type */}
           <FormField
             control={form.control}
             name="type"
             render={({ field }) => (
-              <FormItem className="col-span-6">
+              <FormItem className="col-span-12 md:col-span-6">
                 <FormLabel>Type</FormLabel>
                 <Select
                   onValueChange={field.onChange}
@@ -192,11 +299,12 @@ export default function ColumnCard({ columnData }: { columnData: ColumnData }) {
               </FormItem>
             )}
           />
+          {/* Not Null */}
           <FormField
             control={form.control}
             name="notNull"
             render={({ field }) => (
-              <FormItem className="col-span-2">
+              <FormItem className="col-span-12 sm:col-span-4 md:col-span-2">
                 <FormLabel>Not Null</FormLabel>
                 <FormControl>
                   <ToggleButton
@@ -210,11 +318,12 @@ export default function ColumnCard({ columnData }: { columnData: ColumnData }) {
               </FormItem>
             )}
           />
+          {/* Unique */}
           <FormField
             control={form.control}
             name="unique"
             render={({ field }) => (
-              <FormItem className="col-span-2">
+              <FormItem className="col-span-12 sm:col-span-4 md:col-span-2">
                 <FormLabel>Unique</FormLabel>
                 <FormControl>
                   <ToggleButton
@@ -228,11 +337,12 @@ export default function ColumnCard({ columnData }: { columnData: ColumnData }) {
               </FormItem>
             )}
           />
+          {/* Primary Key */}
           <FormField
             control={form.control}
             name="primaryKey"
             render={({ field }) => (
-              <FormItem className="col-span-2">
+              <FormItem className="col-span-12 sm:col-span-4 md:col-span-2">
                 <FormLabel>Primary Key</FormLabel>
                 <FormControl>
                   <ToggleButton
@@ -240,6 +350,52 @@ export default function ColumnCard({ columnData }: { columnData: ColumnData }) {
                     onChange={field.onChange}
                     disabled={field.disabled}
                     label="Primary Key"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {/* Min/Max */}
+          <FormField
+            control={form.control}
+            name="min"
+            render={({ field }) => (
+              <FormItem className="col-span-12 sm:col-span-4 md:col-span-3">
+                <FormLabel>Min</FormLabel>
+                <FormControl>
+                  <NumberInputWithPlusMinus
+                    value={field.value}
+                    onChange={(value) => {
+                      field.onChange(value)
+                      setTimeout(() => {
+                        form.trigger(['min', 'max'])
+                      }, 0)
+                    }}
+                    disabled={field.disabled}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {/* Max */}
+          <FormField
+            control={form.control}
+            name="max"
+            render={({ field }) => (
+              <FormItem className="col-span-12 sm:col-span-4 md:col-span-3">
+                <FormLabel>Max</FormLabel>
+                <FormControl>
+                  <NumberInputWithPlusMinus
+                    value={field.value}
+                    onChange={(value) => {
+                      field.onChange(value)
+                      setTimeout(() => {
+                        form.trigger(['min', 'max'])
+                      }, 0)
+                    }}
+                    disabled={field.disabled}
                   />
                 </FormControl>
                 <FormMessage />
