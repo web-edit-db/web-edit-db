f8d9f87a9fa4203eb27ff12e02cff683eb8e74f6
Hanan Fokkens <hananfokkens@gmail.com>
2025-09-01 23:46:56 +1200

    feat: add unit tests for column helper functions in SQLite
    - Introduced tests for `deconsturctColumn` and `constructColumn` functions to validate their behavior with various input types and edge cases.
    - Ensured comprehensive coverage for handling types, min, and max values, as well as invalid inputs.

---
+++ app/lib/sqlite/helpers.test.ts
@@ -0,0 +1,101 @@
+import { describe, expect, it } from 'vitest'
+import { constructColumn, deconsturctColumn } from './helpers'
+
+describe('helpers', () => {
+  describe('deconsturctColumn', () => {
+    it.each(['INTEGER', 'TEXT', 'NUMBER', 'BLOB', 'REAL'])(
+      'should pull out the type, for %s, there is no min or max',
+      (type) => {
+        expect(deconsturctColumn(type)).toEqual({
+          type: type.toLowerCase(),
+          min: undefined,
+          max: undefined,
+        })
+      },
+    )
+
+    it.each(['INTEGER(10)', 'TEXT(10)', 'NUMBER(10)', 'BLOB(10)', 'REAL(10)'])(
+      'should pull out the type, for %s, there is only a max',
+      (type) => {
+        expect(deconsturctColumn(type)).toEqual({
+          type: type.substring(0, type.indexOf('(')).toLowerCase(),
+          min: undefined,
+          max: 10,
+        })
+      },
+    )
+
+    it.each(['INTEGER(10, 20)', 'TEXT(10, 20)', 'NUMBER(10, 20)', 'BLOB(10, 20)', 'REAL(10, 20)'])(
+      'should pull out the type, for %s, there is a min and max',
+      (type) => {
+        expect(deconsturctColumn(type)).toEqual({
+          type: type.substring(0, type.indexOf('(')).toLowerCase(),
+          min: 10,
+          max: 20,
+        })
+      },
+    )
+
+    it('should handle an empty string', () => {
+      expect(deconsturctColumn('')).toBeNull()
+    })
+
+    it('should handle an invalid column', () => {
+      expect(deconsturctColumn('*')).toBeNull()
+    })
+
+    it("should handle '(10)'", () => {
+      expect(deconsturctColumn('(10)')).toBeNull()
+    })
+
+    it("should handle '(10, 20)'", () => {
+      expect(deconsturctColumn('(10, 20)')).toBeNull()
+    })
+  })
+
+  describe('constructColumn', () => {
+    it.each([
+      { type: 'integer', min: undefined, max: undefined },
+      { type: 'bool', min: undefined, max: undefined },
+      { type: 'text', min: undefined, max: undefined },
+      { type: 'number', min: undefined, max: undefined },
+      { type: 'blob', min: undefined, max: undefined },
+      { type: 'real', min: undefined, max: undefined },
+    ])('should construct the column for %s, where there is no min or max', ({ type, min, max }) => {
+      expect(constructColumn({ type, min, max })).toBe(type.toUpperCase())
+    })
+
+    it.each([
+      { type: 'integer', min: 10, max: undefined },
+      { type: 'bool', min: 10, max: undefined },
+      { type: 'text', min: 10, max: undefined },
+      { type: 'number', min: 10, max: undefined },
+      { type: 'blob', min: 10, max: undefined },
+      { type: 'real', min: 10, max: undefined },
+    ])('should construct the column for %s, where there is only a max', ({ type, min, max }) => {
+      expect(constructColumn({ type, min, max })).toBe(`${type.toUpperCase()}(${min})`)
+    })
+
+    it.each([
+      { type: 'integer', min: undefined, max: 10 },
+      { type: 'bool', min: undefined, max: 10 },
+      { type: 'text', min: undefined, max: 10 },
+      { type: 'number', min: undefined, max: 10 },
+      { type: 'blob', min: undefined, max: 10 },
+      { type: 'real', min: undefined, max: 10 },
+    ])('should construct the column for %s, where there is only a min', ({ type, min, max }) => {
+      expect(constructColumn({ type, min, max })).toBe(`${type.toUpperCase()}(${max})`)
+    })
+
+    it.each([
+      { type: 'integer', min: 10, max: 20 },
+      { type: 'bool', min: 10, max: 20 },
+      { type: 'text', min: 10, max: 20 },
+      { type: 'number', min: 10, max: 20 },
+      { type: 'blob', min: 10, max: 20 },
+      { type: 'real', min: 10, max: 20 },
+    ])('should construct the column for %s, where there is a min and max', ({ type, min, max }) => {
+      expect(constructColumn({ type, min, max })).toBe(`${type.toUpperCase()}(${min}, ${max})`)
+    })
+  })
+})
--- app/lib/sqlite/helpers.ts
+++ app/lib/sqlite/helpers.ts
@@ -1,6 +1,8 @@
 import type { BindParams, Database } from 'sql.js'
 import { z } from 'zod'
 
+const COLUMN_REGEX = /^(?<type>[A-Za-z]+)(\(((?<min>\d+), )?(?<max>\d*?)\)$)?/
+
 export const parsedQuery = <T extends z.ZodObject>(
   database: Database,
   query: string,
@@ -8,23 +10,28 @@ export const parsedQuery = <T extends z.ZodObject>(
   params?: BindParams,
   limit?: number,
 ): z.infer<T>[] => {
-  const statement = database.prepare(query)
-  // bind params
-  if (params) {
-    statement.bind(params)
-  }
-  const results = [] as unknown[]
-  while (statement.step() && (limit === undefined || results.length < limit)) {
-    const row = statement.getAsObject()
-    results.push(row)
-  }
-  statement.free()
-  const parsedResults = z.array(rowSchema).safeParse(results)
-  if (!parsedResults.success) {
-    console.error(parsedResults.error)
+  try {
+    const statement = database.prepare(query)
+    // bind params
+    if (params) {
+      statement.bind(params)
+    }
+    const results = [] as unknown[]
+    while (statement.step() && (limit === undefined || results.length < limit)) {
+      const row = statement.getAsObject()
+      results.push(row)
+    }
+    statement.free()
+    const parsedResults = z.array(rowSchema).safeParse(results)
+    if (!parsedResults.success) {
+      console.error(parsedResults.error, results)
+      return []
+    }
+    return parsedResults.data
+  } catch (error) {
+    console.error(error, query)
     return []
   }
-  return parsedResults.data
 }
 
 export const parsedQuerySingle = <T extends z.ZodObject>(
@@ -35,4 +42,28 @@ export const parsedQuerySingle = <T extends z.ZodObject>(
 ): z.infer<T> | null => {
   const results = parsedQuery(database, query, rowSchema, params)
   return results.length > 0 ? results[0] : null
+}
+
+export const deconsturctColumn = (column: string) => {
+  const match = column.match(COLUMN_REGEX)
+  if (!match) return null
+  const { type, min, max } = match.groups || {}
+  return {
+    type: type.toLowerCase(),
+    min: min ? Number(min) : undefined,
+    max: max ? Number(max) : undefined,
+  }
+}
+
+export const constructColumn = (column: {
+  type: string
+  min: number | undefined
+  max: number | undefined
+}) => {
+  const type = column.type.toUpperCase()
+  const suffix = `(${[column.min, column.max].filter((value) => value !== undefined).join(', ')})`
+  if (suffix === '()') {
+    return type
+  }
+  return `${type}${suffix}`
 }
